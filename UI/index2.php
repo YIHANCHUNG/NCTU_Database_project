1a91914d96eb90c4c1bd1c5722325b9a1ddff2bf
YI-HAN <64484646+YIHANCHUNG@users.noreply.github.com>
2020-07-12 22:47:53 +0800

    basic search

---
+++ UI/index2.php
@@ -0,0 +1,65 @@
+<?php
+	include 'header.php'
+?>
+
+
+<div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+		<form action="search2.php" method="POST">
+		<input type="text" name="keyword" placeholder="Insert keyword">
+		<input type="checkbox" name="tag[]" value="1">tag<br><br>
+		<div style="display:none">
+		<input type="checkbox" name="tag[]" value="2" checked>
+		</div>
+		category: <select name="category">
+		<option value="0" selected> "None" </option>
+		<option value="1"> "Film & Animation" </option> 
+		<option value="2"> "Autos & Vehicles" </option> 
+		<option value="3"> "Music" </option>  
+		<option value="4"> "Pets & Animals" </option> 
+		<option value="5"> "Sports" </option> 
+		<option value="6"> "Short Movies" </option> 
+		<option value="7"> "Travel & Events" </option>
+		<option value="8"> "Gaming" </option>
+		<option value="9"> "Videoblogging" </option>
+		<option value="10"> "People & Blogs" </option>
+		<option value="11"> "Comedy" </option>
+		<option value="12"> "Entertainment" </option>
+		<option value="13"> "News & Politics" </option>
+		<option value="14"> "Howto & Style" </option>
+		<option value="15"> "Education" </option>
+		<option value="16"> "Science & Technology" </option>
+		<option value="17"> "Nonprofits & Activism" </option>
+		<option value="18"> "Movies" </option>
+		<option value="19"> "Anime/Animation" </option>
+		<option value="20"> "Action/Adventure" </option>
+		<option value="21"> "Classics" </option>
+		<option value="22"> "Comedy" </option>
+		<option value="23"> "Documentary" </option>
+		<option value="24"> "Drama" </option>
+		<option value="25"> "Family" </option>
+		<option value="26"> "Foreign" </option>
+		<option value="27"> "Horror" </option>
+		<option value="28"> "Sci-Fi/Fantasy" </option>
+		<option value="29"> "Thriller" </option>
+		<option value="30"> "Shorts" </option>
+		<option value="31"> "Shows" </option>
+		<option value="32"> "Trailers" </option>
+		</select><br><br>
+		view:
+		<input type="text" name="view_lower" placeholder="lower">
+		~
+		<input type="text" name="view_upper" placeholder="upper"><br><br>
+		like:
+		<input type="text" name="like_lower" placeholder="lower">
+		~
+		<input type="text" name="like_upper" placeholder="upper"><br><br>
+		dislike:
+		<input type="text" name="dislike_lower" placeholder="lower">
+		~
+		<input type="text" name="dislike_upper" placeholder="upper"><br><br>
+		<button type="submit" name="search" value="search">Summit</button>
+	  	
+		</form>
+    </div>
+</div>
---
+++ UI/search2.php
@@ -0,0 +1,91 @@
+<?php
+	include 'header.php';
+?>
+
+<div class="result-container">
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "youtube_db";
+    $connection = mysqli_connect($servername, $username, $password, $db) or die("Error " . mysqli_error($connection));
+    //echo "Connected". "<br>"
+
+	if(isset($_POST['search']))
+	{
+		$keyword = mysqli_real_escape_string($connection, $_POST['keyword']);
+		$tag = $_POST['tag'];
+		$category = $_POST['category'];
+		if($_POST['view_lower'] != '') {
+		    $view_lower = $_POST['view_lower'];
+		} else {
+			$view_lower = 0;
+		}
+		if($_POST['view_upper'] != '') {
+		    $view_upper = $_POST['view_upper'];
+		} else {
+			$view_upper = 2147483647;
+		}
+		if($_POST['like_lower'] != '') {
+		    $like_lower = $_POST['like_lower'];
+		} else {
+			$like_lower = 0;
+		}
+		if($_POST['like_upper'] != '') {
+		    $like_upper = $_POST['like_upper'];
+		} else {
+			$like_upper = 2147483647;
+		}
+		if($_POST['dislike_lower'] != '') {
+		    $dislike_lower = $_POST['dislike_lower'];
+		} else {
+			$dislike_lower = 0;
+		}
+		if($_POST['dislike_upper'] != '') {
+		    $dislike_upper = $_POST['dislike_upper'];
+		} else {
+			$dislike_upper = 2147483647;
+		}
+		
+		if($category == 0) {
+			if(count($tag) > 1) {
+			    $sql = "SELECT * FROM (SELECT * FROM statistic WHERE views BETWEEN $view_lower AND $view_upper AND likes BETWEEN $like_lower AND $like_upper AND dislikes BETWEEN $dislike_lower AND $dislike_upper) as s, (SELECT * FROM basic as b, detail as d WHERE b.video_id = d.video_id AND (b.title LIKE '%$keyword%' OR b.channel_title LIKE '%$keyword%' OR d.tags LIKE '%$keyword%')) as b WHERE b.video_id = s.video_id LIMIT 50";
+		    } else {
+			    $sql = "SELECT * FROM (SELECT * FROM statistic WHERE views BETWEEN $view_lower AND $view_upper AND likes BETWEEN $like_lower AND $like_upper AND dislikes BETWEEN $dislike_lower AND $dislike_upper) as s, (SELECT * FROM basic as b WHERE b.title LIKE '%$keyword%' OR b.channel_title LIKE '%$keyword%') as b WHERE b.video_id = s.video_id LIMIT 50";
+		    }
+		} else {
+			if(count($tag) > 1) {
+			    $sql = "SELECT * FROM (SELECT * FROM statistic WHERE views BETWEEN $view_lower AND $view_upper AND likes BETWEEN $like_lower AND $like_upper AND dislikes BETWEEN $dislike_lower AND $dislike_upper) as s, (SELECT * FROM basic as b, detail as d WHERE b.video_id = d.video_id AND (b.title LIKE '%$keyword%' OR b.channel_title LIKE '%$keyword%' OR d.tags LIKE '%$keyword%') AND d.category_id = $category) as b WHERE b.video_id = s.video_id LIMIT 50";
+		    } else {
+			    $sql = "SELECT * FROM (SELECT * FROM statistic WHERE views BETWEEN $view_lower AND $view_upper AND likes BETWEEN $like_lower AND $like_upper AND dislikes BETWEEN $dislike_lower AND $dislike_upper) as s, (SELECT * FROM basic as b, detail as d WHERE b.video_id = d.video_id AND (b.title LIKE '%$keyword%' OR b.channel_title LIKE '%$keyword%') AND d.category_id = $category) as b WHERE b.video_id = s.video_id LIMIT 50";
+		    }
+		}
+		
+		$result = mysqli_query($connection, $sql);
+		if($result != "")
+			$queryResult = mysqli_num_rows($result);
+		else
+			$queryResult = 0;
+		
+		if($queryResult > 0)
+		{
+			while($row = mysqli_fetch_assoc($result))
+			{
+				echo $row['video_id']." ";
+				echo $row['title']." ";
+				echo $row['channel_title']." ";
+				echo $row['publish_time']." ";
+				echo $row['trending_date']." ";
+				echo $row['views']." ";
+                echo $row['likes']." ";
+				echo $row['dislikes']." ";
+				echo "<a href=&quot;https://www.youtube.com/watch?v='$row'['video_id']&quot;target='_blank'>access video</a><br>";
+			}
+		}
+		else
+		{
+			echo "<br><br>&nbsp There are no results matching your search!";
+		}
+	}
+?>
+</div>
