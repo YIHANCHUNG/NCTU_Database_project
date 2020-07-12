9d523f47393d7ca297db9fe45a8829c812fe5daf
ggurte3364 <50067546+ggurte3364@users.noreply.github.com>
2020-07-12 12:14:30 +0800

    Create csv_content_import.sql

---
+++ Data_Import_SQL/csv_content_import.sql
@@ -0,0 +1,51 @@
+create table db(
+
+    video_id char(11) NOT NULL,
+
+    trending_date char(8) NOT NULL,
+
+    title text NOT NULL,
+
+    channel_title text NOT NULL,
+
+    category_id int NOT NULL,
+
+    publish_time text NOT NULL,
+
+    tags text,
+
+    views int NOT NULL,
+
+    likes int NOT NULL,
+
+    dislikes int NOT NULL,
+
+    comment_count int NOT NULL,
+
+    thumbnail_link text,
+
+    comments_disabled boolean NOT NULL,
+
+    ratings_disabled boolean NOT NULL,
+
+    video_error_or_removed boolean NOT NULL,
+
+    description text
+
+);
+
+
+
+load data local INFILE './USvideos.csv'
+
+into table db
+
+CHARACTER SET latin1
+
+fields terminated by ','
+
+enclosed by '"'
+
+lines terminated by '\n'
+
+ignore 1 lines;
