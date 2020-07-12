b1837d2e845876650f0a58ae828cc61a698f9942
YI-HAN <64484646+YIHANCHUNG@users.noreply.github.com>
2020-07-13 01:31:25 +0800

    still working

--- UI/index_HAHA.php
+++ UI/index_HAHA.php
@@ -7,26 +7,49 @@
     <div class="w3-row w3-padding-64">
       <div class="w3-twothird w3-container">
         <h1 class="w3-text-teal">Select the categories you like</h1>
-          <form method="post">
+          <form method="post" action="index_HAHA2.php">
             <label class="heading">To Select Multiple Options Press ctrl+left click :</label><br>
-            <select multiple name="Color[]" size="5">
-              <option value="Red">Red</option>
-              <option value="Green">Green</option>
-              <option value="Blue">Blue</option>
-              <option value="Pink">Pink</option>
-              <option value="Yellow">Yellow</option>
-              <option value="White">White</option>
-              <option value="Black">Black</option>
-              <option value="Violet">Violet</option>
-              <option value="Limegreen">Limegreen</option>
-              <option value="Brown">Brown</option>
-              <option value="Orange">Orange</option>
+            <select multiple name="category[]" size="5">
+              <option value="1"> "Film & Animation" </option> 
+              <option value="2"> "Autos & Vehicles" </option> 
+              <option value="10"> "Music" </option>  
+              <option value="15"> "Pets & Animals" </option> 
+              <option value="17"> "Sports" </option> 
+              <option value="18"> "Short Movies" </option> 
+              <option value="19"> "Travel & Events" </option>
+              <option value="20"> "Gaming" </option>
+              <option value="21"> "Videoblogging" </option>
+              <option value="22"> "People & Blogs" </option>
+              <option value="23"> "Comedy" </option>
+              <option value="24"> "Entertainment" </option>
+              <option value="25"> "News & Politics" </option>
+              <option value="26"> "Howto & Style" </option>
+              <option value="27"> "Education" </option>
+              <option value="28"> "Science & Technology" </option>
+              <option value="29"> "Nonprofits & Activism" </option>
+              <option value="30"> "Movies" </option>
+              <option value="31"> "Anime/Animation" </option>
+              <option value="32"> "Action/Adventure" </option>
+              <option value="33"> "Classics" </option>
+              <option value="34"> "Comedy" </option>
+              <option value="35"> "Documentary" </option>
+              <option value="36"> "Drama" </option>
+              <option value="37"> "Family" </option>
+              <option value="38"> "Foreign" </option>
+              <option value="39"> "Horror" </option>
+              <option value="40"> "Sci-Fi/Fantasy" </option>
+              <option value="41"> "Thriller" </option>
+              <option value="42"> "Shorts" </option>
+              <option value="43"> "Shows" </option>
+              <option value="44"> "Trailers" </option>
             </select>
-            <?php include'HAHA_select_value.php'; ?>
-            <input name="submit" type="submit" value="Get Selected Values">
-          <form>
-          <br><br><button class="button" onclick="">reset</button>
-
+            <!--<input name="submit" type="submit" value="Get Selected Values">-->
+            <br><br><input name="ok" type="submit" value="OK">
+            <?php #include'./index_HAHA/HAHA_select_value.php'; ?>
+            <?php #include'./index_HAHA/HAHA_OK.php'; ?>
+          </form>
+          
+          <br><br><button onclick="location.href='index_HAHA.php'" style="color:red">reset</button>
       </div>
     </div>
   </body>
---
+++ UI/index_HAHA2.php
@@ -0,0 +1,16 @@
+<?php
+  include 'header.php'
+?>
+
+<html>
+  <body class="w3-main">
+    <div class="w3-row w3-padding-64">
+      <div class="w3-twothird w3-container">
+        <h1 class="w3-text-teal">Choose the videos you like</h1>
+            <?php include'./index_HAHA/HAHA_OK.php'; ?>
+          
+          <br><br><button onclick="location.href='index_HAHA.php'" style="color:red">reset</button>
+      </div>
+    </div>
+  </body>
+</html>
